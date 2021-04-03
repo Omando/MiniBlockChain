a6748318e1e3f86c7fd85c4daf74a6402c8b4a5e
Yazan Diranieh <yazanmd@yahoo.com>
2021-04-03 21:23:53 +0100

    first cut of domain http routing code

--- bid/router.go
+++ bid/router.go
@@ -1,1 +1,15 @@
 package bid
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func NewRouter(nodeAddress string) *mux.Router {
+
+	/* mux.Router matches incoming requests against a list of registered routes and calls
+	a handler for the route that matches the URL or other condition. It implements the
+	http.Handler interface so it is compatible with the standard http.ServeMux.
+	*/
+	var router *mux.Router = mux.NewRouter().StrictSlash(true)
+	return router;
+}
