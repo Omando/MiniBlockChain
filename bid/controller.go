22fec9898d37c8c78f70fd167213db004becff09
Yazan Diranieh <yazanmd@yahoo.com>
2021-04-08 00:09:47 +0100

    controller web api - delta 1

--- bid/controller.go
+++ bid/controller.go
@@ -1,1 +1,21 @@
 package bid
+
+import "net/http"
+
+// Index GET/
+func (c *Controller) Index(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// GetBlockChain GET /blockchain
+func (c *Controller) GetBlockChain(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+// RegisterBid POST/bid
+func (c *Controller) RegisterBid(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+
+
