32a430291b04e007a09be800f69ba5a66dc1ef62
Yazan Diranieh <yazanmd@yahoo.com>
2021-04-29 16:43:33 +0100

    Adding skeletal functions - delta 1

--- bid/blockchain.go
+++ bid/blockchain.go
@@ -1,1 +1,14 @@
 package bid
+
+// RegisterBid registers a bid in the blockchain
+func (b *BlockChain) RegisterBid(bid Bid) bool {
+
+}
+
+// RegisterNode registers a node in the blockchain
+func (b *BlockChain) RegisterNode(node string) bool {
+
+}
+
+
+
