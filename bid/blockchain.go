77974b0c4683d0a484704f529200ad5b5157de62
Yazan Diranieh <yazanmd@yahoo.com>
2021-05-20 00:47:22 +0100

    Code refactor

--- bid/blockchain.go
+++ bid/blockchain.go
@@ -1,5 +1,29 @@
 package bid
 
+import "time"
+
+// CreateNewBlock create new block and appends it to the blockchain
+func (b *BlockChain) CreateNewBlock(nonce int, previousBlockHash string, hash string) Block {
+
+	// Create and initialize a new block
+	newBlock := Block{
+		Index:             len(b.Chain) + 1,	// Length of chain + 1
+		Timestamp:         time.Now().UnixNano(),
+		Bids:              b.PendingBids,
+		Nonce:             nonce	,
+		Hash:              hash,
+		PreviousBlockHash: previousBlockHash,
+	}
+
+	// There are no pending bids when a new block is created
+	b.PendingBids = Bids{}
+
+	// Add this new block to the chain
+	b.Chain = append(b.Chain, newBlock )
+	
+	return  newBlock
+}
+
 // RegisterBid registers a bid in the blockchain
 func (b *BlockChain) RegisterBid(bid Bid) bool {
 	panic("not implemented")
@@ -7,11 +31,6 @@ func (b *BlockChain) RegisterBid(bid Bid) bool {
 
 // RegisterNode registers a node in the blockchain
 func (b *BlockChain) RegisterNode(node string) bool {
-	panic("not implemented")
-}
-
-// CreateNewBlock create new block and appends it to the blockchain
-func (b *BlockChain) CreateNewBlock(nonce int, previousBlockHash string, hash string) Block {
 	panic("not implemented")
 }
 
